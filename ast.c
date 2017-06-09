1e6249eddb6217e64324731c2aa9c9309dce1ac9
Chelo <mlynch@itba.edu.ar>
2017-06-09 12:09:17 -0300

    Refactor

--- ast.c
+++ ast.c
@@ -37,6 +37,10 @@ void produce(Statements * block, char * indent){
 
 
 void doAssign(AssignmentNode * a){
+	char * expression = getArith(a->value);
+	printf("assign(%d, %s);\n", a->var_id, expression);
+	free(expression);
+
 	switch(a->value->type){
 		case STR_TYPE:
 			printf("assign(%d, anon_str(\"%s\"));\n", a->var_id, *((char**)a->value->left));
@@ -58,8 +62,43 @@ void doAssign(AssignmentNode * a){
 }
 
 
+#define STR_OVERHEAD 20
+#define MAX_DIGITS 30
 char* getArith(ArithNode * operation){
+	char * result;
+	switch(operation->type){
+		case STR_TYPE: 
+		{
+			char * str = *((char**)a->value->left);
+			result = malloc(STR_OVERHEAD + strlen(str));
+			sprintf(result, "anon_str(\"%s\")", str);
+			break;
+		}
+		
+		case INT_TYPE:
+		{	
+			result = malloc(STR_OVERHEAD + MAX_DIGITS);
+			sprintf(result, "anon_int(%d)", *((int*)a->value->left));
+			break;
+		}
 
+		case FLOAT_TYPE:
+		{
+			result = malloc(STR_OVERHEAD + MAX_DIGITS);
+			sprintf(result, "anon_int(%30f)", *((float*)a->value->left));
+			break;
+		}
+
+		case ARIT_SUM:
+		{
+			char * left = getArith((ArithNode*)operation->left);
+			char * right = getArith((ArithNode*)operation->right);
+			result = malloc(STR_OVERHEAD + strlen(left) + strlen(right) + 1);
+			sprintf(result, "sum(%s, %s)", left, right);
+
+		}
+	}
+	return result;
 }
 
 void doPrint(PrintNode * p){
