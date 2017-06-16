6ab66f9b39e366f2bd04a2a610bfa30ffea2e8ce
tcerdaITBA <tcerda@itba.edu.ar>
2017-06-16 17:55:03 -0300

    Arreglado algo del while

--- compiled.c
+++ compiled.c
@@ -3,12 +3,16 @@
 #include "prose_functions.h"
 void main(void) { 
 
-assign(0, anon_arr());
-array_add(0, anon_int(6));
-array_add(0, anon_int(7));
-array_add(0, anon_str("hola"));
-assign(1, anon_arr());
-array_add(1, anon_int(10));
-array_add(1, get_var(0));
-print_var(get_var(1), 1);
+assign(0, anon_int(0));
+assign(1, anon_int(10));
+
+
+while((compare(get_var(1), anon_int(0)) > 0)){
+assign(1, var_sub(get_var(1), anon_int(1)));
+assign(0, var_sum(get_var(0), get_var(1)));
+}
+
+print_var(get_var(0), 1);
+print_var(anon_str("Hola mundo"), 1);
+print_var(anon_str("Hola mundo"), 1);
 }
