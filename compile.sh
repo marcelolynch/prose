9616aa7d1abd68ce87baa3a76bc1eaa96b56b734
Chelo <mlynch@itba.edu.ar>
2017-06-17 13:01:39 -0300

    Cambios en makefile

--- compile.sh
+++ compile.sh
@@ -1,13 +1,9 @@
 cd compiler
 make all
-gcc -o prose_compiler y.tab.c ast.c lex.yy.c -ly
-
 
 cd ..
 ./compiler/prose_compiler < $1 > compiled.c
 gcc compiled.c runtime/*.c -o prose.out
 
 cd compiler
-
-rm prose_compiler
 make clean
