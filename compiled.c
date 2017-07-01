bd4030a5ab951fd854486e10c1d86ce85d8db1f6
Chelo <mlynch@itba.edu.ar>
2017-06-30 21:39:10 -0300

    Agrego informe

--- compiled.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include "runtime/include/variables.h"
-#include "runtime/include/prose_arrays.h"
-#include "runtime/include/prose_functions.h"
-int main(void) { 
-
-print_var(var_div(anon_str("abcdef"), anon_int(3)), 1);
-}
